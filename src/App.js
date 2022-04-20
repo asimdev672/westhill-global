5d8d90b7014ee32792a5a45394b23d91726cbe63
Asimgi <asim152038@gmail.com>
2022-04-20 14:42:30 +0500

    Add background

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 // import Lgnavbar from './components/Lgnavbar'
 import Navbar from './components/Navbar';
 import SectionB from './components/SectionB';
+import Footer from './components/Footer';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 
@@ -11,6 +12,7 @@ function App() {
  {/* <Lgnavbar/> */}
    <Navbar/>
    <SectionB/>
+   <Footer/>
     </>
   );
 }
--- src/components/SectionB.jsx
+++ src/components/SectionB.jsx
@@ -3,10 +3,12 @@ import './SectionB.css'
 const SectionB = () => {
   return (
     <>
+    <div className="container-fluid bg-color">
     <div className="container">
       <p className='para'>Let's face it: the current home repair process is difficult.</p>
       <p className='para'>Unfortunately, homeowners often have to give up valuable time and become project managers to ensure that repairs get completed - countless research hours to find a reputable contractor, deciphering scope of work, trying to track every bit of new information, and ultimately ensuring that they are not being taken advantage of.</p>
       <p className='para'>We believe that there is a better way. Westhill exists to improve and streamline the repair process for homeowners. By leveraging technology, Westhill helps homeowners:</p>
+    </div>
     </div>
       {/*------------- 1st div-------------------------- */}
       <div className="container-fluid px-0 ">
@@ -28,9 +30,9 @@ const SectionB = () => {
               </div>
           </div>
           <div className='center-div'>
-            <h2 className='fw-bolder mt-5 py-lg-4 text-center mb-0 '>
+            <h4 className='fw-bold mt-5 py-lg-4 text-center mb-0 '>
             Westhill is the first, and leading fully-digital managed repair solution connecting  insurance carriers, homeowners, and highly-qualified reputable contractors.
-            </h2>
+            </h4>
           </div>
       </div>
             {/*------------- 2nd div-------------------------- */}
