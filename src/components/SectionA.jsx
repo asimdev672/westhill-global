414b9383ff23c78aed04b555b88a291bd78b7fe3
Asimgi <asim152038@gmail.com>
2022-04-21 16:25:20 +0500

    data send through props

--- src/components/SectionA.jsx
+++ src/components/SectionA.jsx
@@ -1,17 +1,12 @@
 import React from "react";
-import "./SectionA.css";
+import Video from "./Video";
 const SectionA = () => {
   return (
     <>
-      <video width="100%" height="auto" autoPlay muted loop >
-      {/* <video  autoPlay muted loop id="videolayout"> */}
-        <source src="img/bg video.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-      <section className="overlay">
-          <p className="fw-bold fs-1"> <strong>WELCOME TO WESTHILL</strong> </p>
-          <p className="small-text">THE FATEST AND EASY WAY</p>
-      </section>
+    <Video 
+        stext='WELCOME TO WESTHILL'
+        ptext='THE FATEST AND EASY WAY'
+    />
     </>
   );
 };
---
+++ src/components/Video.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Video.css'
+const Video = (props) => {
+  return (
+    <>
+       <video width="100%" height="auto" autoPlay muted loop >
+      {/* <video  autoPlay muted loop id="videolayout"> */}
+        <source src="img/bg video.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <section className="overlay">
+          <p className="fw-bold fs-1"> <strong>{props.stext}</strong> </p>
+          <p className="small-text">{props.ptext}</p>
+      </section>
+    </>
+  )
+}
+
+export default Video
